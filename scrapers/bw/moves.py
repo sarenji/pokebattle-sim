76dd178602711af0376c893bb3af374c744bbd9b
David Peter <david.a.peter@gmail.com>
2012-08-25 21:28:26 -0400

    Add damage type to each move JSON.
    Accuracy in move JSON is also fixed. Blank strings become 0.

--- scrapers/bw/moves.py
+++ scrapers/bw/moves.py
@@ -5,14 +5,26 @@ import collections
 output_path = '../../data/bw/data_moves.json'
 moves = {}
 types = {}
+damage_types = {}
 
 moves_url = 'https://raw.github.com/veekun/pokedex/master/pokedex/data/csv/moves.csv'
 type_names_url = 'https://raw.github.com/veekun/pokedex/master/pokedex/data/csv/types.csv'
+damage_types_url = 'https://raw.github.com/veekun/pokedex/master/pokedex/data/csv/move_damage_classes.csv'
 
 Move = collections.namedtuple('Move', ["id", "identifier", "generation_id", 
   "type_id", "power", "pp", "accuracy", "priority", "target_id", 
   "damage_class_id", "effect_id", "effect_chance", "contest_type_id", 
   "contest_effect_id", "super_contest_effect_id"])
+
+# Parse damage types
+lines = requests.get(damage_types_url).text.splitlines()
+lines.pop(0) # get rid of info
+
+while len(lines) > 0:
+  line = lines.pop(0)
+  damage_type_id, identifier = line.split(',')
+  damage_types[damage_type_id] = identifier
+
 
 # Parse types
 lines = requests.get(type_names_url).text.splitlines()
@@ -34,8 +46,9 @@ while len(lines) > 0:
     'type'     : types[move.type_id],
     'power'    : int(move.power),
     'pp'       : move.pp and int(move.pp),
-    'accuracy' : move.accuracy and int(move.accuracy),
+    'accuracy' : (move.accuracy and int(move.accuracy)) or 0,
     'priority' : int(move.priority),
+    'damage'   : damage_types[move.damage_class_id],
   }
   
 with open(output_path, 'w') as f:
