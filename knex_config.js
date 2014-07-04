82073737b5ba14a283828d35e70528981c9d9de0
David Peter <dpeter@kickstarter.com>
2014-07-04 13:09:20 -0400

    Support using DATABASE_URL

--- knex_config.js
+++ knex_config.js
@@ -1,3 +1,18 @@
+var connection = {};
+
+if (process.env.DATABASE_URL) {
+  var url = require('url').parse(process.env.DATABASE_URL);
+  connection.host = url.host;
+  connection.database = decodeURI(url.pathname.slice(1));
+  connection.user = url.auth[0];
+  connection.password = url.auth[1];
+} else {
+  connection.host = process.env.APP_DB_HOST;
+  connection.user = process.env.APP_DB_USER;
+  connection.password = process.env.APP_DB_PASSWORD;
+  connection.database = process.env.APP_DB_NAME;
+}
+
 module.exports = {
   directory: './migrations',
   tableName: 'migrations',
@@ -5,10 +20,10 @@ module.exports = {
   database: {
     client: 'postgresql',
     connection: {
-      host     : process.env.APP_DB_HOST     || '127.0.0.1',
-      user     : process.env.APP_DB_USER     || 'postgres',
-      password : process.env.APP_DB_PASSWORD || '',
-      database : process.env.APP_DB_NAME     || 'pokebattle_sim'
+      host     : connection.host     || '127.0.0.1',
+      user     : connection.user     || 'postgres',
+      password : connection.password || '',
+      database : connection.database || 'pokebattle_sim'
     }
   }
 }
